416a4d8fcd12b1ce70109c9f83086ab7c11a628c
Sheenan Ahsan <sheenanahsan@gmail.com>
2024-07-13 20:08:53 -0400

    first commit

---
+++ src/monkey/token/token.go
@@ -0,0 +1,35 @@
+package token
+
+type TokenType string
+
+type Token struct {
+	Type TokenType
+	Literal string
+}
+
+const (
+	// Special types
+	ILLEGAL = "ILLEGAL" // token/character we don't know
+	EOF = "EOF" // end of file
+
+	// Identifiers + literals
+	IDENT = "IDENT" // add, foobar, x, y, ...
+	INT = "INT" // 1343456
+
+	// Operators
+	ASSIGN = "="
+	PLUS = "+"
+
+	// Delimiters
+	COMMA = ","
+	SEMICOLON = ";"
+
+	LPAREN = "("
+	RPAREN = ")"
+	LBRACE = "{"
+	RBRACE = "}"
+
+	// Keywords
+	FUNCTION = "FUNCTION"
+	LET = "LET"
+)
